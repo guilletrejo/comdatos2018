4a8050766557cc665995d93950e2cadd4ec9ede8
AndresSalvatierrra <andressalvatierra11@gmail.com>
2018-09-05 15:26:49 -0300

    Punto3
    Former-commit-id: 9ad6b94def9337a735facfbe6c27893da8d6c089

---
+++ Tp1/Punto3/cliente_ftp.py
@@ -0,0 +1,17 @@
+from ftplib import FTP
+
+ftp = FTP('')
+
+ftp.connect('192.168.1.16',1026)
+ftp.login()
+ftp.cwd('/Facu/Comunicaciones_de_Datos/comdatos2018/Tp1/Punto3') #replace with your directory
+ftp.retrlines('LIST')
+
+def downloadFile():
+ filename = 'hola.txt' #replace with your file in the directory ('directory_name')
+ localfile = open(filename, 'wb')
+ ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
+ ftp.quit()
+ localfile.close()
+
+downloadFile()
