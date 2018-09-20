1890ca8fe4df7a5a2536d6e4e02e31ad2299e0f7
AndresSalvatierrra <andressalvatierra11@gmail.com>
2018-09-20 19:40:07 -0300

    Imagenes punto2
    Former-commit-id: 8bfff32012d10fabf34fe7f0948e6516d9dad3be
    Former-commit-id: beee7c328a93bc6103bc104e32d0ed6383e9f16a

--- Tp1/Punto2/serverUDP.py
+++ Tp1/Punto2/serverUDP.py
@@ -8,12 +8,12 @@ UDP_IP= ""
 UDP_PORT= 5006
 
 #Creación del Socket,AF_INET socket de internet(IPv4),SOCK_DGRAM orientado a protocolo UDP
-client_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+server_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
                                                               
 #Enlazo el socket a una direccion IP y un puerto(donde estará "escuchando")                                                             
-client_socket.bind((UDP_IP,UDP_PORT))
+server_socket.bind((UDP_IP,UDP_PORT))
 
 while True:
 #Obtenemos la direccion y el puerto desde la que se hace la petición
-    data, addr = client_socket.recvfrom(4096) #Tamaño del buffer 4096
+    data, addr = server_socket.recvfrom(4096) #Tamaño del buffer 4096
     print ("received message: ", data.decode())
